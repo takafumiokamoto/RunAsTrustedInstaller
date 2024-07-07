35954be77b3c7d6189632fd6dc336a26fcc437da
Takafumi Okamoto <takafumi.okamoto.work@gmail.com>
2024-07-07 15:26:18 +0900

    trustedinstaller serviceが起動しているかの確認から、起動していなければ起動する。

--- main.cpp
+++ main.cpp
@@ -34,8 +34,11 @@ DWORD SetDebugPrivilege() {
 	}
 }
 
-
-void PrintError(DWORD err) {
+/*
+* GetLastError()の内容をコンソール出力する。
+*/
+void Error() {
+	auto err = GetLastError();
 	wchar_t* msgBuf = nullptr;
 	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, err, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (wchar_t*)&msgBuf, 0, NULL);
 	_bstr_t b(msgBuf); const char* c = b;
@@ -43,6 +46,9 @@ void PrintError(DWORD err) {
 	LocalFree(msgBuf);
 }
 
+/*
+* 実行ファイル名からpidを取得する。
+*/
 DWORD GetPidByName(const wchar_t* processName) {
 	DWORD processId = 0;
 	// 現在のプロセス全体のスナップショットを作成
@@ -64,10 +70,38 @@ DWORD GetPidByName(const wchar_t* processName) {
 	return processId;
 }
 
+/*
+* pidに対応するtokenハンドラーを取得する。
+*/
 HANDLE GetProcessToken(DWORD pid) {
+	HANDLE hCurrentProcess;
+	if (pid != 0) {
+		// pidに対応するプロセスのハンドルを取得
+		hCurrentProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, TRUE, pid);
+		if (hCurrentProcess == NULL) {
+			// 失敗した場合
+			Error();
+			return INVALID_HANDLE_VALUE;
+		}
+	}
+	else {
+		hCurrentProcess = GetCurrentProcess();
+	}
+	HANDLE hToken;
+	//	https://learn.microsoft.com/ja-jp/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocesstoken#parameters
+	bool success = OpenProcessToken(hCurrentProcess, TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY, &hToken);
+	if (!success) {
+		Error();
+		return INVALID_HANDLE_VALUE;
+	}
+	CloseHandle(hCurrentProcess);
+	return hToken;
 
 }
 
+/*
+* pidに対応するアクセストークンを複製する。
+*/
 HANDLE DuplicateProcessToken(DWORD pid, TOKEN_TYPE tokenType) {
 	// プロセスIDからアクセストークンを取得
 	HANDLE hToken = GetProcessToken(pid);
@@ -75,13 +109,12 @@ HANDLE DuplicateProcessToken(DWORD pid, TOKEN_TYPE tokenType) {
 		return INVALID_HANDLE_VALUE;
 	}
 	//https://learn.microsoft.com/ja-jp/windows-hardware/drivers/ddi/wdm/ne-wdm-_security_impersonation_level
-	//リモートではないので
 	SECURITY_IMPERSONATION_LEVEL seImpersonateLevel = SecurityImpersonation;
 	HANDLE hNewToken = {};
 	//https://learn.microsoft.com/ja-jp/windows/win32/api/securitybaseapi/nf-securitybaseapi-duplicatetokenex
 	// アクセストークンの複製
 	if (!DuplicateTokenEx(hToken, MAXIMUM_ALLOWED, NULL, seImpersonateLevel, tokenType, &hNewToken)) {
-		PrintError(GetLastError());
+		Error();
 		CloseHandle(hToken);
 		return INVALID_HANDLE_VALUE;
 	}
@@ -93,15 +126,34 @@ int wmain() {
 	bool success = SetDebugPrivilege();
 	if (!success) {
 		// 管理者権限で実行されていない
-		PrintError(GetLastError());
+		Error();
 	}
 	DWORD pid = GetPidByName(L"winlogon.exe");
 	if (pid == 0) return -1;
-	// winlogon.exeはTruestedInstallerで起動している。
-	// そのプロセスIDをキーにアクセストークンを取得し複製する。
-	// TOKEN_TYPEはTokenPrimaryかTokenImpersonationどちらでもよいはず
-	// 偽装トークンで十分なはずなので、TokenImpersonation
-	HANDLE hImpToken = DuplicateProcessToken(pid, TOKEN_TYPE::TokenImpersonation);
-	// 複製したトークンをスポーンさせるプロセスに割り当てる。
+	//winlogon.exeのキーにアクセストークンを取得し偽装トークンとして複製する。
+	HANDLE hImpersonationToken = DuplicateProcessToken(pid, TOKEN_TYPE::TokenImpersonation);
+	if (hImpersonationToken == INVALID_HANDLE_VALUE) {
+		printf("winlogon.exeのアクセストークンの複製に失敗しました。");
+		return -1;
+	}
+	// https://learn.microsoft.com/ja-jp/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentthread
+	HANDLE hCurrentThread = GetCurrentThread();
+	// 複製した偽装トークンを現在のスレッドに割り当てる。
+	bool success = SetThreadToken(&hCurrentThread, hImpersonationToken);
+	if (!success) {
+		Error();
+		return -1;
+	}
+	CloseHandle(hCurrentThread);
+	CloseHandle(hImpersonationToken);
+	//https://learn.microsoft.com/ja-jp/windows/win32/api/winsvc/nf-winsvc-openservicew
+	//https://learn.microsoft.com/ja-jp/windows/win32/api/winsvc/nf-winsvc-openscmanagerw
+	SC_HANDLE hService = OpenServiceW(OpenSCManagerW(NULL, NULL, SC_MANAGER_ALL_ACCESS), L"trustedinstaller", MAXIMUM_ALLOWED);
+	if (hService == NULL) {
+		// trustedinstaller serviceのハンドルを取得できなかった。
+		Error();
+		return -1;
+	}
+
 
 }
